fd1cc2bf32f971bf88a8b7c8c8bf8a79bd78d2df
Dylan Clements <dclements@protonmail.com>
2019-01-11 17:28:16 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+    pigeon_list = {}
+ data.each do |trait, trait_list|
+    trait_list.each do |specified_trait,names|
+        names.each do |pigeon_name|
+            pigeon_list[pigeon_name] = {}
+            pigeon_list[bird_name][pigeon_attribute] = []
+            pigeon_list[bird_name][pigeon_attribute] << trait_list
+        end
+    end
+end
+pigeon_list
 end
